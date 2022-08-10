7e93b49bd8697c174290f7c3b74b73348538e219
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-11 00:44:21 +0530

    Refactored : Test to check if result sync with database.

--- test/java/com/bridgelabz/JDBCTest/EmployeePayrollJDBCTest.java
+++ test/java/com/bridgelabz/JDBCTest/EmployeePayrollJDBCTest.java
@@ -2,6 +2,7 @@ package com.bridgelabz.JDBCTest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -37,6 +38,23 @@ public class EmployeePayrollJDBCTest {
 		Statement statement = connection.createStatement();
 		statement.execute(EmployeePayrollJDBC.updateData);
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll where Name = 'Rahul'");
+		Double result = 0.0;
+		while (resultSet.next()) {
+			result = resultSet.getDouble(7);
+		}
+		Double expexted = 300000.0;
+		Assert.assertEquals(expexted, result);
+	}
+
+	/*
+	 * UC 4 - Test to compare results of prepared Statement with payroll_service
+	 * database
+	 */
+	@Test
+	public void given_UpdateQueryPreparedStatementShouldReturnTrue_ResultShouldSyncWithDatabase() throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement(EmployeePayrollJDBC.updateData);
+		preparedStatement.execute(EmployeePayrollJDBC.updateData);
+		ResultSet resultSet = preparedStatement.executeQuery("SELECT * FROM employee_payroll where Name = 'Terissa'");
 		Double result = 0.0;
 		while (resultSet.next()) {
 			result = resultSet.getDouble(7);
