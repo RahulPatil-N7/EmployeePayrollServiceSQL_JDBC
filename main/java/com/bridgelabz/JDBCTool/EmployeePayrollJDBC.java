4ec592b1a35bc2947690b4e89f0aad9593456960
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-10 23:53:09 +0530

    Added : connecting to payroll_service database.

---
+++ main/java/com/bridgelabz/JDBCTool/EmployeePayrollJDBC.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.JDBCTool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ *Java program to connect payroll_service database.
+ */
+public class EmployeePayrollJDBC {
+	public static void main(String[] args) throws SQLException {
+		System.out.println("************Welcome To Employee Payroll Service************\n");
+		// UC1 - Connecting to database
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String passWord = "Rahul@N77";
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+			System.out.println("Connecting to database : " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, passWord);
+			System.out.println("Connection is successful!" + connection);
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		} finally {
+			connection.close();
+		}
+	}
+
+}
